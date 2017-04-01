4dc2ba718c690a49e93cfdc33ad9ed15ea74c5c2
root <zhengengyang@126.com>
2017-04-01 14:44:09 +0800

    first commit

---
+++ countpath.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "fmt"
+)
+
+func count(tower [][]int) int {
+    for i := len(tower) - 1; i >= 0; i-- {
+        for j := 0; j < i; j++ {
+            if tower[i][j] > tower[i][j+1] {
+                tower[i-1][j] +=tower[i][j]
+            } else {
+                tower[i-1][j] +=tower[i][j+1]
+           }
+        }
+    }
+    return tower[0][0]
+
+}
+
+func main() {
+    var tower = [][]int{
+                 {9,0,0,0},
+                 {12,15,0,0},
+                 {10,6,8,0},
+                 {2,18,9,5},
+                }
+    fmt.Println(count(tower))
+
+}
