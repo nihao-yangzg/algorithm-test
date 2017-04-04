c600f75c82e5325326ba8c7aa803f5f010eeae12
nihao-yangzg <zhengengyang@126.com>
2017-04-04 16:21:28 +0800

    graph add color

---
+++ backtracking/graphcolor.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+    "fmt"
+)
+
+/**
+ * 无向连通图着色问题
+ */
+
+func color(cur int) int {
+    var num = 0
+    if cur == len(graph) {
+        return 1
+    }
+    for i := 0; i < len(colors); i++ {
+        result[cur] = colors[i]
+        ok := true
+        for j := 0; j < len(graph); j++ {
+            point := graph[cur][j]
+            if point != 0 && result[cur] == result[j] {
+                ok = false
+                break
+            }
+        }
+        if (ok) {
+           next := cur + 1
+           num += color(next)
+        }
+    }
+    return num
+
+}
+
+var graph = [][]int{
+        {0,1,1,1,0,0},
+        {1,0,0,0,0,0},
+        {1,0,0,1,1,1},
+        {1,0,1,0,0,0},
+        {0,0,1,0,0,0},
+        {0,0,1,0,0,0},
+    }
+
+var colors = []int{1,2,3,4,5}
+var result = []int{0,0,0,0,0,0}
+func main(){
+    num := color(0)
+    fmt.Println(num)
+}
