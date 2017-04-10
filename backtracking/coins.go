ff7c179030638a9c610f994bae165cfacce74a64
nihao-yangzg <zhengengyang@126.com>
2017-04-10 17:35:16 +0800

    edits coins

--- backtracking/coins.go
+++ backtracking/coins.go
@@ -8,9 +8,9 @@ import (
 )
 
 var coins = []int{2,3,5,7}
-var target = 16
+var target = 15
 var result [][]int
-func find(subrst []int, summary int, index int){
+func find(subrst []int, summary int, index int, max int){
     for i := index; i < len(coins); i++ {
         subrst = append(subrst, coins[i])
         sum := summary + coins[i]
@@ -20,12 +20,13 @@ func find(subrst []int, summary int, index int){
             subrst = subrst[0 : len(subrst) - 1]
             continue
         } else {
-            find(subrst, sum, i)
+            find(subrst, sum, i, max)
         }
         //开辟新的空间，并将原来的元素拷贝进入新空间，最后一个元素忽略
-        temp := []int{}
+        temp := make([]int, len(subrst) - 1, max)
         for j := 0; j < len(subrst) - 1; j++ {
-            temp = append(temp, subrst[j])
+            // temp = append(temp, subrst[j])
+            temp[j] = subrst[j]
         }
         subrst = temp
         
@@ -33,8 +34,15 @@ func find(subrst []int, summary int, index int){
 }
 
 func main(){
-    var subrst = []int{}
-    find(subrst, 0, 0)
+    var min = coins[0]
+    for i := 1; i < len(coins); i++ {
+        if coins[i] < min {
+            min = coins[i]
+        }
+    }
+    var subrstMaxLength = target / min
+    var subrst = []int{} 
+    find(subrst, 0, 0, subrstMaxLength)
     for i := 0; i < len(result); i++ {
         fmt.Println(result[i])
     }
